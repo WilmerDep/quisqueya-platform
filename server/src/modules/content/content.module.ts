6c2b701b9d15aed7b30575ab4769bd5e5cf059f7
wilmer jimenez <108029465+WilmerDep@users.noreply.github.com>
2026-07-31 17:30:30 -0400

    feat(content): register DMC content reader

--- server/src/modules/content/content.module.ts
+++ server/src/modules/content/content.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContentController } from './content.controller.js';
 import { ContentService } from './content.service.js';
+import { DmcContentService } from './dmc-content.service.js';
 
 @Module({
   controllers: [ContentController],
-  providers: [ContentService],
-  exports: [ContentService],
+  providers: [ContentService, DmcContentService],
+  exports: [ContentService, DmcContentService],
 })
 export class ContentModule {}
