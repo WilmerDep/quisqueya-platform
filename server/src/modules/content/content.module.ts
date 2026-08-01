5ec2f9279ab9fb3c0d6b7188b616c64a10fb325d
wilmer jimenez <108029465+WilmerDep@users.noreply.github.com>
2026-07-31 20:49:39 -0400

    feat(content): register reviews controllers and service

--- server/src/modules/content/content.module.ts
+++ server/src/modules/content/content.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ContentController } from './content.controller.js';
 import { ContentService } from './content.service.js';
 import { DmcContentService } from './dmc-content.service.js';
+import { PublicReviewsController, ReviewsController } from './reviews.controller.js';
+import { ReviewsService } from './reviews.service.js';
 
 @Module({
-  controllers: [ContentController],
-  providers: [ContentService, DmcContentService],
-  exports: [ContentService, DmcContentService],
+  controllers: [ContentController, PublicReviewsController, ReviewsController],
+  providers: [ContentService, DmcContentService, ReviewsService],
+  exports: [ContentService, DmcContentService, ReviewsService],
 })
 export class ContentModule {}
