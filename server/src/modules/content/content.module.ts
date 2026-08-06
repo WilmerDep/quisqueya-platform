736d19ad0433de14737abd0c5e8e570aa2b23293
wilmer jimenez <108029465+WilmerDep@users.noreply.github.com>
2026-08-05 20:13:23 -0400

    feat(content): register experience editorial API

--- server/src/modules/content/content.module.ts
+++ server/src/modules/content/content.module.ts
@@ -3,13 +3,20 @@ import { AuthModule } from '../auth/auth.module.js';
 import { ContentController } from './content.controller.js';
 import { ContentService } from './content.service.js';
 import { DmcContentService } from './dmc-content.service.js';
+import { ExperienceEditorialController } from './experience-editorial.controller.js';
+import { ExperienceEditorialService } from './experience-editorial.service.js';
 import { PublicReviewsController, ReviewsController } from './reviews.controller.js';
 import { ReviewsService } from './reviews.service.js';
 
 @Module({
   imports: [AuthModule],
-  controllers: [ContentController, PublicReviewsController, ReviewsController],
-  providers: [ContentService, DmcContentService, ReviewsService],
-  exports: [ContentService, DmcContentService, ReviewsService],
+  controllers: [
+    ContentController,
+    ExperienceEditorialController,
+    PublicReviewsController,
+    ReviewsController,
+  ],
+  providers: [ContentService, DmcContentService, ExperienceEditorialService, ReviewsService],
+  exports: [ContentService, DmcContentService, ExperienceEditorialService, ReviewsService],
 })
 export class ContentModule {}
