95955c84da242ee03b88b24026262a6c410545c9
wilmer jimenez <108029465+WilmerDep@users.noreply.github.com>
2026-09-08 17:57:31 -0400

    Register content preview controller and service

--- server/src/modules/content/content.module.ts
+++ server/src/modules/content/content.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module.js';
 import { ContentController } from './content.controller.js';
 import { ContentService } from './content.service.js';
+import { ContentPreviewController } from './content-preview.controller.js';
+import { ContentPreviewService } from './content-preview.service.js';
 import { DmcContentService } from './dmc-content.service.js';
 import { ExperienceEditorialController } from './experience-editorial.controller.js';
 import { ExperienceEditorialService } from './experience-editorial.service.js';
@@ -12,11 +14,12 @@ import { ReviewsService } from './reviews.service.js';
   imports: [AuthModule],
   controllers: [
     ContentController,
+    ContentPreviewController,
     ExperienceEditorialController,
     PublicReviewsController,
     ReviewsController,
   ],
-  providers: [ContentService, DmcContentService, ExperienceEditorialService, ReviewsService],
-  exports: [ContentService, DmcContentService, ExperienceEditorialService, ReviewsService],
+  providers: [ContentService, ContentPreviewService, DmcContentService, ExperienceEditorialService, ReviewsService],
+  exports: [ContentService, ContentPreviewService, DmcContentService, ExperienceEditorialService, ReviewsService],
 })
 export class ContentModule {}
